6cc6e999ce24943240c7ff4c397a0b41a2b4f352
kessless <kessless@foxmail.com>
2023-03-12 00:08:11 +0800

    add express

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
 const TELEGRAM_BOT_TOKEN = '5843596200:AAFfeDlX2b2Oko9iJjjQ5fF8S6yMZO_ct0s'
 const URL = 'telegram-bot-telegram-bot-55d2.up.railway.app'
@@ -5,6 +6,21 @@ const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
 
 
 bot.setWebHook(`${URL}/bot${TELEGRAM_BOT_TOKEN}`);
+
+const app = express();
+
+app.use(express.json());
+
+// We are receiving updates at the route below!
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Start Express Server
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
+});
 
 bot.onText(/导航/,msg => {
   const {id} = msg.chat
