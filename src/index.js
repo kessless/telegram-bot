b903e3e80d3eaee95c52bed505d92df270147489
kessless <kessless@foxmail.com>
2023-03-12 00:03:01 +0800

    update node-telegram-bot-api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-const TeleBot = require('telebot');
+const TelegramBot = require('node-telegram-bot-api');
 const TELEGRAM_BOT_TOKEN = '5843596200:AAFfeDlX2b2Oko9iJjjQ5fF8S6yMZO_ct0s'
-const bot = new TeleBot(TELEGRAM_BOT_TOKEN);
-bot.on('text', (msg) => msg.reply.text(msg.text));
+const URL = 'telegram-bot-telegram-bot-55d2.up.railway.app'
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
 
-bot.start();
+
+bot.setWebHook(`${URL}/bot${TELEGRAM_BOT_TOKEN}`);
+
+bot.onText(/导航/,msg => {
+  const {id} = msg.chat
+  bot.sendMessage(id,'hello world')
+})
+
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'I am alive!');
+});
