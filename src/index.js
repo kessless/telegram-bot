6ff70616c8accf1392650e8354ccb5f046aead9e
kessless <kessless@foxmail.com>
2023-03-14 22:28:33 +0800

    fixbugsssss

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ bot.onText(/^导航$/, async msg => {
   //     inline_keyboard: buttons
   //   }
   // })
-  console.log('xh---------buttons',buttons)
+  console.log('xh---------buttons',buttons[0])
   bot.sendMessage(id, `${buttons}`)
 })
 
