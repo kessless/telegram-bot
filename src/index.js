fb87e1d96def1ffc6c4c556d08e79f0d2a8a6368
xh01469600 <xh01469600@lazada.com>
2023-03-15 15:19:20 +0800

    test command

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const TELEGRAM_BOT_TOKEN = '5843596200:AAFfeDlX2b2Oko9iJjjQ5fF8S6yMZO_ct0s'
 const URL = 'telegram-bot-telegram-bot-55d2.up.railway.app'
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN);
 
+const botName = 'kessless_bot'
+
 
 bot.setWebHook(`${URL}/bot${TELEGRAM_BOT_TOKEN}`);
 
@@ -41,20 +43,23 @@ bot.onText(/^设置导航$/, ({ from, chat }) => {
   const { id: userId } = from
   const { type, id } = chat
   if (type === 'private') {
-    console.log('xh---type', type)
     bot.sendMessage(id, '设置导航', {
       reply_markup: {
-        inline_keyboard: [[{ text: '选择一个你所管理的群组', url: 't.me/kessless_bot?startgroup=false' }]]
+        inline_keyboard: [[{ text: '选择一个你所管理的群组', url: `t.me/${botName}?startgroup=false` }]]
       }
     })
   }
 });
+bot.onText(/^\/start/, ({ message_id, from, chat }) => {
+  const { type, id } = chat
+  if (type !== 'private') {
+    bot.deleteMessage(id, message_id)
+  }
+});
+
+
 
 bot.setMyCommands([{ command: '/start', description: '设置机器人' }])
-
-bot.on('inline_query', (...args) => {
-  console.log('xh------msg', args)
-})
 
 
 bot.on('webhook_error', (error) => {
